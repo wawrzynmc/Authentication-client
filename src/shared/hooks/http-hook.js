3074d49473d6facde96a94d12b45554950ee190b
wawrzynmc <karol.wawrzenczyk@gmail.com>
2020-09-18 15:44:06 +0200

    :sparkles: http hook

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,59 @@
+import { useState, useCallback, useRef, useEffect } from 'react';
+
+export const useHttpClient = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
+
+	const activeHttpRequests = useRef([]);
+
+	const sendRequest = useCallback(
+		async (url, method = 'GET', body = null, headers = {}) => {
+			setIsLoading(true);
+			const httpAbortCtrl = new AbortController(); // prevent error if you send request and change the page
+			activeHttpRequests.current.push(httpAbortCtrl);
+
+			try {
+				const response = await fetch(url, {
+					method,
+					body,
+					headers,
+					signal: httpAbortCtrl.signal,
+				});
+
+				const responseData = await response.json();
+
+				activeHttpRequests.current = activeHttpRequests.current.filter(
+					(reqCtrl) => reqCtrl !== httpAbortCtrl
+				);
+
+				if (!response.ok) {
+					// .ok exists on 200 like status codes
+					throw new Error(responseData.message);
+				}
+
+				setIsLoading(false);
+				return responseData;
+			} catch (err) {
+				setError(err.message);
+				setIsLoading(false);
+				throw err;
+			}
+		},
+		[]
+	);
+
+	const clearError = () => {
+		setError(null);
+	};
+
+	useEffect(() => {
+		// run on unmount component, or before next execution of useEffect function
+		return () => {
+			activeHttpRequests.current.forEach((abortCtrl) =>
+				abortCtrl.abort()
+			);
+		};
+	}, []);
+
+	return { isLoading, error, sendRequest, clearError };
+};
