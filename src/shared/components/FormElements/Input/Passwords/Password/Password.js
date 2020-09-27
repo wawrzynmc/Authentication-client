9af17de0686b5900ee8fab8e732a65dedbfee6bb
wawrzynmc <karol.wawrzenczyk@gmail.com>
2020-09-27 12:41:15 +0200

    :construction: Work in progress with Password Component
    Create password component and restructure folder structure

---
+++ src/shared/components/FormElements/Input/Passwords/Password/Password.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+// * -- my own imports
+// ---- components
+import PasswordMeter from '../PasswordMeter/PasswordMeter';
+
+// ---- functions
+import { validate } from '../../../../../utils/validators';
+import { capitalizeString } from '../../../../../utils/utils';
+import {
+	VALIDATOR_PASSWORD,
+} from '../../../../../utils/validators';
+
+// ---- styles
+import classes from './Password.module.scss';
+
+const Password = (props) => {
+    return()
+}
+
+export default Password
