34124dd34b2bdef16c90599d8d6de4b1ce594f1b
wawrzynmc <karol.wawrzenczyk@gmail.com>
2020-10-16 12:33:55 +0200

    :memo: Documentation for Logo component

--- src/shared/components/Navigation/Logo/Logo.js
+++ src/shared/components/Navigation/Logo/Logo.js
@@ -1,24 +1,39 @@
+// * -- libraries imports
 import React from 'react';
+import PropTypes from 'prop-types';
 
+// * -- my own imports
+// ---- assets
 import logo from '../../../../assets/images/logo.png';
 
+// ---- styles
 import classes from './Logo.module.scss';
 
+/**
+ * Render Logo component
+ * @category Navigation
+ * @component
+ */
 const Logo = (props) => {
-	let attachedClasses = [classes.Logo];
-
-	if (props.navigation) {
-		attachedClasses.push(classes.NavigationLogo);
-	}
-
 	return (
 		<div
-			className={attachedClasses.join(' ')}
+			className={`
+				${classes.Logo}
+				${props.navigation ? classes.NavigationLogo : null}
+			`}
 			style={{ height: props.height }}
 		>
 			<img src={logo} alt="logo" />
 		</div>
 	);
+};
+
+// * -- prop types
+Logo.propTypes = {
+	/**Defines special style if logo will be placed in navigation */
+	navigation: PropTypes.bool,
+	/**Defines heigth of element */
+	height: PropTypes.string,
 };
 
 export default Logo;
